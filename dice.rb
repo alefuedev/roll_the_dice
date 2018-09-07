664e326b6d263960ff5f88468db7773d6cb5e516
ale <alefuentesdev@gmail.com>
2018-09-07 13:12:40 -0400

    doubles.rb, add and if and else if the numbers are equal Doubles! will show up else the result!

---
+++ dice.rb
@@ -0,0 +1,4 @@
+dice_one = Random.rand(1..6)
+dice_two = Random.rand(1..6)
+p "Your rolled #{dice_one} and #{dice_two}."
+p "Your total is #{dice_one + dice_two}."
---
+++ doubles.rb
@@ -0,0 +1,9 @@
+dice_one = Random.rand(1..6)
+dice_two = Random.rand(1..6)
+p "Your rolled #{dice_one} and #{dice_two}."
+
+if dice_one == dice_two
+	p "Doubles!"
+else
+	p "Your total is #{dice_one + dice_two}."
+end
