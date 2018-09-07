c650a28310d4b762ca94e27553439decf371c7d6
ale <alefuentesdev@gmail.com>
2018-09-07 15:37:41 -0400

    how_many.rb, use of the method length over permutations variable to count how many permutations are possible

---
+++ how_many.rb
@@ -0,0 +1,23 @@
+dice_one = [1,2,3,4,5,6]
+dice_two = [1,2,3,4,5,6]
+dice = []
+
+dice_one.each do |num|
+	dice.push(num)
+end
+
+dice_two.each do |num|
+	dice.push(num)
+end
+
+permutations = dice.permutation(2).to_a.sort
+
+permutations = permutations.uniq
+
+permutations.each do |comb|
+	result = 0
+	comb.each { |num|result+=num }
+		print "Dice Roll: #{comb} Total:#{result}"
+		puts
+end
+p "There are #{permutations.length} possible permutations."
